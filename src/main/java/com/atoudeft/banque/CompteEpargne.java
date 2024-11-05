36180ab832afb6b939ac02ef3214d2feab4efc27
SamuelFerragne <samuel.ferragne@hotmail.com>
2024-11-05 16:38:33 -0500

    Fait: 4.1 Ajout de la classe CompteEpargne et implementation des methodes heritées et ajouterInterets

---
+++ src/main/java/com/atoudeft/banque/CompteEpargne.java
@@ -0,0 +1,58 @@
+package com.atoudeft.banque;
+
+public class CompteEpargne extends CompteBancaire{
+    static final double frais = 2; //Frais de 2$
+    static final double limiteMinimum = 1000; //Solde minimum sans frais
+
+    private String numero;
+    private TypeCompte type;
+    private double solde;
+    private double interets; // Interets en pourcentage
+
+    /**
+     * Crée un compte bancaire.
+     *
+     * @param numero numéro du compte
+     * @param interets taux d'interets du compte epargne
+     */
+    public CompteEpargne(String numero, double interets) {
+        super(numero, TypeCompte.EPARGNE);
+        this.interets = interets;
+    }
+
+    @Override
+    public boolean crediter(double montant) {
+        if(montant > 0) {
+            solde += montant;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean debiter(double montant) {
+        if(montant > 0 && solde >= montant) {
+            solde -= montant;
+            if(solde < CompteEpargne.limiteMinimum) {
+                solde -= CompteEpargne.frais;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean payerFacture(String numeroFacture, double montant, String description) {
+        return false;
+    }
+
+    @Override
+    public boolean transferer(double montant, String numeroCompteDestinataire) {
+        return false;
+    }
+
+    public boolean ajouterInterets() {
+        solde += (solde * (interets / 100));
+        return true;
+    }
+}
